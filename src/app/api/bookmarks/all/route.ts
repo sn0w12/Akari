193519333000d67cb11c3bab8176734631208af3
sn0w12 <lucasskold49@gmail.com>
2024-09-30 04:07:32 +0200

    use local api for getting all bookmarks

--- src/app/api/bookmarks/all/route.ts
+++ src/app/api/bookmarks/all/route.ts
@@ -1,29 +1,19 @@
 import { NextResponse } from 'next/server';
 
-const BOOKMARK_SERVER_URL_1 = 'https://user.mngusr.com/bookmark_get_list_full';
-
 // Function to fetch bookmarks from the external API
-async function fetchBookmarks(user_data: string, page: number, url: string) {
-  const data = new URLSearchParams();
-  data.append('user_data', user_data);
-  data.append('bm_page', page.toString());
-  data.append('bm_source', 'manganato');
-  data.append('out_type', 'json');
-
-  const response = await fetch(url, {
-    method: 'POST',
-    body: data.toString(),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
+async function fetchBookmarks(user_data: string, page: number) {
+  const response = await fetch(
+    `http://localhost:3000/api/bookmarks?page=${page}&user_data=${user_data}`
+  );
 
   if (!response.ok) {
     const errorText = await response.text();
     throw new Error(`HTTP error ${response.status}: ${errorText}`);
   }
 
-  return response.json();
+  const jsonResponse = await response.json();
+  jsonResponse['result'] = 'ok';
+  return jsonResponse;
 }
 
 // Named export for GET requests (you can also use POST if needed)
@@ -38,32 +28,27 @@ export async function GET(request: Request) {
     );
   }
 
-  const url = BOOKMARK_SERVER_URL_1;
-
   const readableStream = new ReadableStream({
     async start(controller) {
       let currentPage = 1;
 
       try {
         while (true) {
-          const result = await fetchBookmarks(user_data, currentPage, url);
+          const result = await fetchBookmarks(user_data, currentPage);
 
           if (result.result !== 'ok') {
             controller.error(new Error('Error fetching bookmarks'));
             return;
           }
 
-          const bookmarks = result.data.map((bookmark: any) => ({
-            ...bookmark,
-            page: currentPage,
-          }));
+          const bookmarks = result.bookmarks;
 
           for (const bookmark of bookmarks) {
             const data = `data: ${JSON.stringify(bookmark)}\n\n`;
             controller.enqueue(new TextEncoder().encode(data));
           }
 
-          const finalPage = result.bm_page_total;
+          const finalPage = result.totalPages;
 
           if (currentPage >= finalPage) {
             const stopEvent = `event: stop\ndata: \n\n`;
