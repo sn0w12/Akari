905acfaf0bfeddaf2a92bbc2485ccd06e5769498
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-01 02:39:42 +0200

    Add author page

---
+++ src/app/author/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { HeaderComponent } from "@/components/Header";
+import AuthorPage from "@/components/Author";
+
+interface PageProps {
+  params: { id: string };
+}
+
+export default function Home({ params }: PageProps) {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <HeaderComponent />
+      <AuthorPage
+        params={{
+          id: params.id,
+        }}
+      />
+    </div>
+  );
+}
