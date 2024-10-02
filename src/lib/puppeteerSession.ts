d58b9e7c11f588be5b4bab60b157be403fd58411
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-02 16:47:55 +0200

    Try to fix puppeteer errors

--- src/lib/puppeteerSession.ts
+++ src/lib/puppeteerSession.ts
@@ -1,9 +1,19 @@
-// puppeteerSession.ts
-import puppeteer from 'puppeteer';
-import type { Browser, Page } from 'puppeteer';
+import type { Browser, Page } from 'puppeteer-core';
 
 let browserInstance: Browser | null = null;
 let pageInstance: Page | null = null;
+
+let chrome = {};
+let puppeteer: { launch: (arg0: { headless: boolean; args: string[]; }) => Browser | PromiseLike<Browser | null> | null; };
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  // running locally.
+  puppeteer = require('puppeteer');
+}
 
 export async function getPuppeteerSession() {
   if (!browserInstance || !pageInstance) {
@@ -11,7 +21,9 @@ export async function getPuppeteerSession() {
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
-    pageInstance = await browserInstance.newPage();
+    if (browserInstance) {
+      pageInstance = await browserInstance.newPage();
+    }
   }
   return { browserInstance, pageInstance };
 }
