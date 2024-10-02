b17545085d8dd52d1543f3b858cf16ad206b49a4
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-02 17:02:05 +0200

    Try to fix puppeteer errors

--- src/lib/puppeteerSession.ts
+++ src/lib/puppeteerSession.ts
@@ -3,12 +3,12 @@ import type { Browser, Page } from 'puppeteer-core';
 let browserInstance: Browser | null = null;
 let pageInstance: Page | null = null;
 
-let chrome = {};
-let puppeteer: { launch: (arg0: { headless: boolean; args: string[]; }) => Browser | PromiseLike<Browser | null> | null; };
+let chromium: { args: string[]; executablePath: any; };
+let puppeteer: { launch: (arg0: { headless: boolean; args: string[]; executablePath?: string; }) => Browser | PromiseLike<Browser | null> | null; };
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   // running on the Vercel platform.
-  chrome = require('chrome-aws-lambda');
+  chromium = require('@sparticuz/chromium');
   puppeteer = require('puppeteer-core');
 } else {
   // running locally.
@@ -19,7 +19,12 @@ export async function getPuppeteerSession() {
   if (!browserInstance || !pageInstance) {
     browserInstance = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: process.env.AWS_LAMBDA_FUNCTION_VERSION || process.env.VERCEL
+        ? chromium.args
+        : ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env.AWS_LAMBDA_FUNCTION_VERSION || process.env.VERCEL
+        ? await chromium.executablePath
+        : undefined,
     });
     if (browserInstance) {
       pageInstance = await browserInstance.newPage();
