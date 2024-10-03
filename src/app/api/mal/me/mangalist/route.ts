1828ca75d2803ee19425efa1a5c807a799857185
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-03 22:26:19 +0200

    Add post to manga list

--- src/app/api/mal/me/mangalist/route.ts
+++ src/app/api/mal/me/mangalist/route.ts
@@ -1,17 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest) {
-  const { access_token } = await req.json();
-
-  if (!access_token) {
-    return NextResponse.json({ error: 'Missing access_token' }, { status: 400 });
+export async function GET(req: NextRequest) {
+  const authorizationHeader = req.headers.get('authorization');
+  if (!authorizationHeader) {
+    return NextResponse.json({ error: 'Authorization header missing' }, { status: 401 });
   }
 
   const response = await fetch(
-    "https://api.myanimelist.net/v2/users/@me/mangalist",
+    "https://api.myanimelist.net/v2/users/@me/mangalist?fields=list_status&sort=list_score&limit=1000",
     {
       headers: {
-        Authorization: `Bearer ${access_token}`,
+        Authorization: authorizationHeader,
       },
     }
   );
@@ -20,6 +19,39 @@ export async function POST(req: NextRequest) {
   if (response.ok) {
     return NextResponse.json(data);
   } else {
+    console.log(data)
     return NextResponse.json({ error: 'Failed to get manga list' }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const { manga_id, num_chapters_read } = await req.json();
+  const authorizationHeader = req.headers.get('authorization');
+  if (!authorizationHeader) {
+    return NextResponse.json({ error: 'Authorization header missing' }, { status: 401 });
+  }
+  if (!manga_id || !num_chapters_read) {
+    return NextResponse.json({ error: 'Missing input' }, { status: 400 });
+  }
+
+  const response = await fetch(
+    `https://api.myanimelist.net/v2/manga/${manga_id}/my_list_status`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: authorizationHeader,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        num_chapters_read,
+      }),
+    }
+  );
+
+  const data = await response.json();
+  if (response.ok) {
+    return NextResponse.json(data);
+  } else {
+    return NextResponse.json({ error: 'Failed to update manga list' }, { status: 500 });
   }
 }
