ee4079bfe0a7e454cf441e13434fc58c60604bf2
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-04 15:19:14 +0200

    get baseUrl from hostname

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,13 +1,4 @@
-let baseUrl: string;
-
-async function fetchBaseUrl() {
-    const response = await fetch("/api/base-url");
-    const data = await response.json();
-    baseUrl = data.baseUrl;
-}
-
-(async () => {
-    await fetchBaseUrl();
-})();
-
-export { baseUrl };
+export const baseUrl =
+    window.location.hostname === "localhost"
+        ? "http://localhost:3000"
+        : `https://${window.location.hostname}`;
