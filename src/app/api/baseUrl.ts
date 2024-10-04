7a1b1fc2bcb6248e460413a89eee846368036911
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-04 15:38:53 +0200

    check VERCEL_ENV

--- src/app/api/baseUrl.ts
+++ src/app/api/baseUrl.ts
@@ -1,8 +1,11 @@
 export function getBaseUrl() {
-    if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-        return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
-    } else if (process.env.VERCEL_URL) {
-        return `https://${process.env.VERCEL_URL}`;
+    const vercelEnv = process.env.VERCEL_ENV;
+    if (vercelEnv) {
+        if (vercelEnv === "production") {
+            return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+        } else {
+            return `https://${process.env.VERCEL_URL}`;
+        }
     } else {
         return "http://localhost:3000";
     }
