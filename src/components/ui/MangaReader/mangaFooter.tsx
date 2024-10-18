b413f29bec70336577ba78c0f583b0abe15db431
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-18 16:20:19 +0200

    Update styling

--- src/components/ui/MangaReader/mangaFooter.tsx
+++ src/components/ui/MangaReader/mangaFooter.tsx
@@ -35,14 +35,14 @@ export default function MangaFooter({ chapterData }: { chapterData: Chapter }) {
                             );
                             window.location.href = newUrl;
                         }}
-                        className="mt-2 mb-2"
+                        className="mt-2 mb-2 w-auto"
                     />
                 </div>
                 <div className="flex items-center gap-4">
                     {lastChapterExists ? (
                         <Link
                             href={`/manga/${chapterData.lastChapter}`}
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
+                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-28 px-4 py-2"
                             aria-label="Previous Chapter"
                         >
                             <ChevronLeft className="mr-2 h-4 w-4" />
@@ -50,7 +50,7 @@ export default function MangaFooter({ chapterData }: { chapterData: Chapter }) {
                         </Link>
                     ) : (
                         <span
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm h-9 px-4 py-2 opacity-50 cursor-not-allowed"
+                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
                             aria-label="Previous Chapter"
                             aria-disabled="true"
                             tabIndex={-1}
@@ -62,7 +62,7 @@ export default function MangaFooter({ chapterData }: { chapterData: Chapter }) {
                     {nextChapterExists ? (
                         <Link
                             href={`/manga/${chapterData.nextChapter}`}
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
+                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 w-28 px-4 py-2"
                             aria-label="Next Chapter"
                         >
                             Next
@@ -70,7 +70,7 @@ export default function MangaFooter({ chapterData }: { chapterData: Chapter }) {
                         </Link>
                     ) : (
                         <span
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow h-9 px-4 py-2 opacity-50 cursor-not-allowed"
+                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
                             aria-label="Next Chapter"
                             aria-disabled="true"
                             tabIndex={-1}
