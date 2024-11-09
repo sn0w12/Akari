b878e0a7bcefd8215674ea509e6fdc27f618acf0
sn0w12 <lucasskold49@gmail.com>
2024-11-09 03:23:11 +0100

    Fix accidentally getting the default setting

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -138,7 +138,7 @@ export function getSetting(key: keyof SettingsInterface) {
         const storedSetting = localStorage.getItem("settings");
         if (storedSetting) {
             const settings = JSON.parse(storedSetting);
-            return settings[key] || defaultSettings[key];
+            return settings[key] ?? defaultSettings[key];
         }
     }
     return null;
