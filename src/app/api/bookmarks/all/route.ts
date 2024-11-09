14ebb8bb42c8da7918f5ad51fd17ce3905e68a1d
Lucas  Sköld <lucasskold49@gmail.com>
2024-11-10 00:15:38 +0100

    Remove timings

--- src/app/api/bookmarks/all/route.ts
+++ src/app/api/bookmarks/all/route.ts
@@ -8,12 +8,10 @@ export const maxDuration = 20;
 const BATCH_SIZE = 10;
 
 export async function GET() {
-    console.time("total-execution");
     const cookieStore = cookies();
     const user_data = getUserData(cookieStore);
 
     if (!user_data) {
-        console.timeEnd("total-execution");
         return NextResponse.json(
             { message: "user_data is required" },
             { status: 400 },
@@ -21,15 +19,12 @@ export async function GET() {
     }
 
     try {
-        console.time("cookie-processing");
         const cookieHeader = cookieStore
             .getAll()
             .map((cookie) => `${cookie.name}=${cookie.value}`)
             .join("; ");
-        console.timeEnd("cookie-processing");
 
         // First fetch to get total pages
-        console.time("first-page-fetch");
         const firstPageResult = await fetch(
             `${getBaseUrl()}/api/bookmarks?page=1`,
             {
@@ -38,8 +33,6 @@ export async function GET() {
         );
 
         if (!firstPageResult.ok) {
-            console.timeEnd("first-page-fetch");
-            console.timeEnd("total-execution");
             return NextResponse.json(
                 { message: "Error fetching bookmarks" },
                 { status: 500 },
@@ -48,12 +41,9 @@ export async function GET() {
 
         const firstPageData = await firstPageResult.json();
         const totalPages = firstPageData.totalPages;
-        console.log(`Total pages to fetch: ${totalPages}`);
         const allBookmarks: Bookmark[] = [...firstPageData.bookmarks];
-        console.timeEnd("first-page-fetch");
 
         // Create batches of page numbers
-        console.time("batch-processing");
         const remainingPages = Array.from(
             { length: totalPages - 1 },
             (_, i) => i + 2,
@@ -66,7 +56,6 @@ export async function GET() {
 
         // Fetch batches in parallel
         for (const batch of batches) {
-            console.time(`batch-${batch[0]}-${batch[batch.length - 1]}`);
             const batchPromises = batch.map((page) =>
                 fetch(`${getBaseUrl()}/api/bookmarks?page=${page}`, {
                     headers: { Cookie: cookieHeader },
@@ -77,14 +66,9 @@ export async function GET() {
 
             const batchResults = await Promise.all(batchPromises);
             allBookmarks.push(...batchResults.flat());
-            console.timeEnd(`batch-${batch[0]}-${batch[batch.length - 1]}`);
         }
-        console.timeEnd("batch-processing");
-
-        console.timeEnd("total-execution");
         return NextResponse.json({ bookmarks: allBookmarks }, { status: 200 });
     } catch (error) {
-        console.timeEnd("total-execution");
         return NextResponse.json(
             {
                 message: "Error fetching bookmarks",
