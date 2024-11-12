cf2567216e07c8440939aa620c20535d41aea063
Lucas  Sköld <lucasskold49@gmail.com>
2024-11-12 01:38:51 +0100

    Fix build error

--- src/app/(manga-reader)/manga/[id]/[subId]/page.tsx
+++ src/app/(manga-reader)/manga/[id]/[subId]/page.tsx
@@ -1,16 +1,14 @@
 import ChapterReader from "@/components/MangaReader";
 
 interface MangaReaderProps {
-    params: {
-        id: string;
-        subId: string;
-    };
+    params: Promise<{ id: string; subId: string }>;
 }
 
 export default async function MangaReaderPage({ params }: MangaReaderProps) {
+    const mangaParams = await params;
     return (
         <div className="min-h-dvh bg-background text-foreground">
-            <ChapterReader id={params.id} subId={params.subId} />
+            <ChapterReader id={mangaParams.id} subId={mangaParams.subId} />
         </div>
     );
 }
