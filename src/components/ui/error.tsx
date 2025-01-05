1b78d0fed50ff941f48a7c9c29b50394eab4ddd9
sn0w12 <lucasskold49@gmail.com>
2025-01-05 18:34:29 +0100

    Make client component

--- src/components/ui/error.tsx
+++ src/components/ui/error.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { AlertCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
