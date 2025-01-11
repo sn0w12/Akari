e6addc8ae80b723032e8a0cb48f06ce111fca0a7
Lucas  Sköld <lucasskold49@gmail.com>
2025-01-11 03:16:47 +0100

    Stack footer buttons when screen is small

--- src/components/ui/MangaReader/mangaFooter.tsx
+++ src/components/ui/MangaReader/mangaFooter.tsx
@@ -45,53 +45,57 @@ export default function MangaFooter({
                         className="mt-2 mb-2 w-auto max-w-64"
                     />
                 </div>
-                <div className="flex items-center gap-4">
-                    <Button
-                        className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent text-accent-foreground h-9 w-28 px-4 py-2"
-                        onClick={toggleReaderMode}
-                    >
-                        Toggle Reader
-                    </Button>
-                    {lastChapterExists ? (
-                        <Link
-                            href={`/manga/${chapterData.lastChapter}`}
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-28 px-4 py-2"
-                            aria-label="Previous Chapter"
+                <div className="flex flex-col w-64 md:w-auto md:flex-row gap-4">
+                    <div className="flex items-center gap-4">
+                        <Button
+                            className="inline-flex flex-grow items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent text-accent-foreground h-9 w-28 px-4 py-2"
+                            onClick={toggleReaderMode}
                         >
-                            <ChevronLeft className="mr-2 h-4 w-4" />
-                            Previous
-                        </Link>
-                    ) : (
-                        <span
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
-                            aria-label="Previous Chapter"
-                            aria-disabled="true"
-                            tabIndex={-1}
-                        >
-                            <ChevronLeft className="mr-2 h-4 w-4" />
-                            Previous
-                        </span>
-                    )}
-                    {nextChapterExists ? (
-                        <Link
-                            href={`/manga/${chapterData.nextChapter}`}
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 w-28 px-4 py-2"
-                            aria-label="Next Chapter"
-                        >
-                            Next
-                            <ChevronRight className="ml-2 h-4 w-4" />
-                        </Link>
-                    ) : (
-                        <span
-                            className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
-                            aria-label="Next Chapter"
-                            aria-disabled="true"
-                            tabIndex={-1}
-                        >
-                            Next
-                            <ChevronRight className="ml-2 h-4 w-4" />
-                        </span>
-                    )}
+                            Toggle Reader
+                        </Button>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        {lastChapterExists ? (
+                            <Link
+                                href={`/manga/${chapterData.lastChapter}`}
+                                className="flex-grow inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-28 px-4 py-2"
+                                aria-label="Previous Chapter"
+                            >
+                                <ChevronLeft className="mr-2 h-4 w-4" />
+                                Previous
+                            </Link>
+                        ) : (
+                            <span
+                                className="flex-grow inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
+                                aria-label="Previous Chapter"
+                                aria-disabled="true"
+                                tabIndex={-1}
+                            >
+                                <ChevronLeft className="mr-2 h-4 w-4" />
+                                Previous
+                            </span>
+                        )}
+                        {nextChapterExists ? (
+                            <Link
+                                href={`/manga/${chapterData.nextChapter}`}
+                                className="flex-grow inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 w-28 px-4 py-2"
+                                aria-label="Next Chapter"
+                            >
+                                Next
+                                <ChevronRight className="ml-2 h-4 w-4" />
+                            </Link>
+                        ) : (
+                            <span
+                                className="flex-grow inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow h-9 w-28 px-4 py-2 opacity-50 cursor-not-allowed"
+                                aria-label="Next Chapter"
+                                aria-disabled="true"
+                                tabIndex={-1}
+                            >
+                                Next
+                                <ChevronRight className="ml-2 h-4 w-4" />
+                            </span>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
