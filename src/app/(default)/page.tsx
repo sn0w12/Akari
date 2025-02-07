7f0dbd8c801457c8fa33a94ec6985e0f2d0e3d29
Lucas  Sköld <lucasskold49@gmail.com>
2025-02-07 14:50:03 +0100

    Add metadata

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,4 +1,5 @@
 import MangaReaderHome from "@/components/Home";
+import { Metadata } from "next";
 
 interface HomeProps {
     searchParams: Promise<{
@@ -6,6 +7,35 @@ interface HomeProps {
         [key: string]: string | string[] | undefined;
     }>;
 }
+
+export const metadata: Metadata = {
+    title: "Akari Manga",
+    description: "Read manga for free on Akari.",
+    openGraph: {
+        title: "Akari Manga",
+        description: "Read manga for free on Akari.",
+        images: [
+            {
+                url: "https://raw.githubusercontent.com/sn0w12/Akari/refs/heads/master/public/img/icon.png",
+                width: 512,
+                height: 512,
+                alt: "Akari Manga",
+            },
+        ],
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@Akari",
+        title: "Akari Manga",
+        description: "Read manga for free on Akari.",
+        images: {
+            url: "https://raw.githubusercontent.com/sn0w12/Akari/refs/heads/master/public/img/icon.png",
+            width: 512,
+            height: 512,
+            alt: "Akari Manga",
+        },
+    },
+};
 
 export default async function Home(props: HomeProps) {
     const searchParams = await props.searchParams;
