3013bb9e618882d9244827f52ddfc110eee1f107
sn0w12 <lucasskold49@gmail.com>
2025-02-24 20:14:35 +0100

    Updata analytics script

--- src/components/ui/analyticsWrapper.tsx
+++ src/components/ui/analyticsWrapper.tsx
@@ -3,12 +3,21 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { useCookieConsent } from "@/hooks/useCookieConsent";
+import Script from "next/script";
 
 export function AnalyticsWrapper() {
     const { consent } = useCookieConsent();
 
     return (
         <>
+            {consent.analytics && (
+                <Script
+                    defer
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="53600aa5-bffc-4e98-8f78-2351d1206043"
+                    strategy="afterInteractive"
+                />
+            )}
             <Analytics
                 beforeSend={(event) => {
                     if (!consent.analytics) {
