ed6c284cc6dc5ea5588b988e88ec78def6a3df0d
Sn0w123 <lucasskold49@gmail.com>
2025-05-10 00:30:01 +0200

    Fix searching

--- src/app/api/(scraping)/search/simple/route.ts
+++ src/app/api/(scraping)/search/simple/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { generateCacheHeaders } from "@/lib/cache";
+import { fetchSearchData } from "@/lib/scraping";
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
@@ -13,30 +14,7 @@ export async function GET(request: Request) {
     }
 
     try {
-        const response = await fetch(
-            `https://${process.env.NEXT_MANGA_URL}/home/search/json?searchword=${encodeURIComponent(query.replaceAll(" ", "_"))}`,
-            {
-                headers: {
-                    "User-Agent":
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
-                    Accept: "application/json, text/javascript, */*; q=0.01",
-                    "Accept-Language": "en-US,en;q=0.5",
-                    "Accept-Encoding": "gzip, deflate, br, zstd",
-                    "Content-Type":
-                        "application/x-www-form-urlencoded; charset=UTF-8",
-                    "X-Requested-With": "XMLHttpRequest",
-                    "Sec-GPC": "1",
-                    Connection: "keep-alive",
-                    Host: `${process.env.NEXT_MANGA_URL}`,
-                    Referer: `https://${process.env.NEXT_MANGA_URL}/`,
-                    "Sec-Fetch-Dest": "empty",
-                    "Sec-Fetch-Mode": "cors",
-                    "Sec-Fetch-Site": "same-origin",
-                    TE: "trailers",
-                },
-            },
-        );
-        const data = await response.json();
+        const data = await fetchSearchData(query);
         return NextResponse.json(data, {
             headers: {
                 ...generateCacheHeaders(600),
