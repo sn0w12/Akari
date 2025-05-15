7563aee78bf2e4cabc7386311a188d7944720e0e
Lucas Sköld <lucasskold49@gmail.com>
2025-05-15 12:55:53 +0200

    Make home skeleton the same as the real home

--- src/components/ui/Home/HomeSkeleton.tsx
+++ src/components/ui/Home/HomeSkeleton.tsx
@@ -7,7 +7,7 @@ import MangaCardSkeleton from "./MangaCardSkeleton";
 export default function HomeSkeleton({ currentPage }: { currentPage: number }) {
     return (
         <>
-            <div className="mx-auto px-4 py-1">
+            <div className="mx-auto px-4 pt-1 pb-4">
                 {currentPage === 1 && (
                     <>
                         <h2 className="text-3xl font-bold mb-6">
