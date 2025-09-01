6ef64505a3d532791d4e492bc0a0cacd1b999dc6
sn0w <105451515+sn0w12@users.noreply.github.com>
2025-09-02 01:09:55 +0200

    Create custom trace sampler

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,34 +1,62 @@
 import { registerOTel } from "@vercel/otel";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import {
     AlwaysOnSampler,
     TraceIdRatioBasedSampler,
+    type Sampler,
+    SamplingDecision,
+    type SamplingResult,
 } from "@opentelemetry/sdk-trace-node";
-import type { IncomingMessage } from "http";
+import { Attributes, Context, Link, SpanKind } from "@opentelemetry/api";
 
-const name: string =
+const name =
     process.env.NEXT_PUBLIC_AKARI_PREVIEW === "1" ? "akari-preview" : "akari";
 
-const ignoreIncomingRequestHook = (request: IncomingMessage): boolean => {
-    return request.url?.includes("_next") ?? false;
-};
+class IgnoreNextSampler implements Sampler {
+    private readonly originalSampler: Sampler;
 
-export function register(): void {
+    constructor(originalSampler: Sampler) {
+        this.originalSampler = originalSampler;
+    }
+
+    shouldSample(
+        context: Context,
+        traceId: string,
+        spanName: string,
+        spanKind: SpanKind,
+        attributes: Attributes,
+        links: Link[],
+    ): SamplingResult {
+        if (spanName.includes("_next")) {
+            return {
+                decision: SamplingDecision.NOT_RECORD,
+            };
+        }
+        return this.originalSampler.shouldSample(
+            context,
+            traceId,
+            spanName,
+            spanKind,
+            attributes,
+            links,
+        );
+    }
+}
+
+const baseSampler: Sampler =
+    process.env.NODE_ENV === "development"
+        ? new AlwaysOnSampler()
+        : new TraceIdRatioBasedSampler(0.1);
+
+const customSampler: Sampler = new IgnoreNextSampler(baseSampler);
+
+export function register() {
     registerOTel({
         serviceName: name,
-        instrumentations: [
-            new HttpInstrumentation({
-                ignoreIncomingRequestHook,
-            }),
-        ],
         instrumentationConfig: {
             fetch: {
                 ignoreUrls: [/_next/],
             },
         },
-        traceSampler:
-            process.env.NODE_ENV === "development"
-                ? new AlwaysOnSampler()
-                : new TraceIdRatioBasedSampler(0.1),
+        traceSampler: customSampler,
     });
 }
