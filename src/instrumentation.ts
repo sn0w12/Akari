452824eef7705c71a5f912f380c8f84e11704701
sn0w <105451515+sn0w12@users.noreply.github.com>
2025-09-02 00:58:30 +0200

    Update instrumentations

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,15 +1,26 @@
 import { registerOTel } from "@vercel/otel";
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 import {
     AlwaysOnSampler,
     TraceIdRatioBasedSampler,
 } from "@opentelemetry/sdk-trace-node";
+import type { IncomingMessage } from "http";
 
-const name =
+const name: string =
     process.env.NEXT_PUBLIC_AKARI_PREVIEW === "1" ? "akari-preview" : "akari";
 
-export function register() {
+const ignoreIncomingRequestHook = (request: IncomingMessage): boolean => {
+    return request.url?.includes("_next") ?? false;
+};
+
+export function register(): void {
     registerOTel({
         serviceName: name,
+        instrumentations: [
+            new HttpInstrumentation({
+                ignoreIncomingRequestHook,
+            }),
+        ],
         instrumentationConfig: {
             fetch: {
                 ignoreUrls: [/_next/],
