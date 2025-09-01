88953f8d03f1dc4fdc3fc318efa773f97b468c53
sn0w <105451515+sn0w12@users.noreply.github.com>
2025-09-01 18:03:15 +0200

    Enable image optimization

--- next.config.js
+++ next.config.js
@@ -63,7 +63,7 @@ module.exports = {
                 pathname: "/api/image-proxy",
             },
         ],
-        unoptimized: true,
+        unoptimized: false,
     },
     experimental: {
         reactCompiler: true,
