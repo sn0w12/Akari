4d629c788b1c8560babd542bbbeffff52f133c0a
Lucas Sköld <lucasskold49@gmail.com>
2025-10-31 21:27:42 +0100

    Add NEXT_PUBLIC_API_URL

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -2,9 +2,8 @@ import createClient from "openapi-fetch";
 import type { paths } from "@/types/api";
 import { inDevelopment } from "@/config";
 
-const baseUrl = inDevelopment
-    ? "http://localhost:5188/"
-    : "http://localhost:5188/";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5188/";
+const baseUrl = inDevelopment ? "http://localhost:5188/" : apiUrl;
 export const client = createClient<paths>({ baseUrl, credentials: "include" });
 
 export const serverHeaders = {
