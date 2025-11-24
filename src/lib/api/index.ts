c52176f08e22c862c582508ecde7943ba4f33f37
Lucas Sköld <lucasskold49@gmail.com>
2025-11-24 23:32:30 +0100

    Add custom user agent for akari website

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,7 @@
 import createClient from "openapi-fetch";
 import type { paths } from "@/types/api";
-import { inDevelopment } from "@/config";
+import { inDevelopment, inPreview } from "@/config";
+import pkg from "../../../package.json";
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5188/";
 const baseUrl = inDevelopment ? "http://localhost:5188/" : apiUrl;
@@ -8,4 +9,7 @@ export const client = createClient<paths>({ baseUrl, credentials: "include" });
 
 export const serverHeaders = {
     "X-API-Key": process.env.API_KEY || "",
+    "user-agent": `AkariWebsite/${pkg.version}/${
+        inPreview ? "preview" : "production"
+    }`,
 };
