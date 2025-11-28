e65962bac684e878c349e3daec27263430ed418e
Lucas Sköld <lucasskold49@gmail.com>
2025-11-28 20:28:28 +0100

    Fix bookmark skeleton

--- src/components/bookmarks/skeleton.tsx
+++ src/components/bookmarks/skeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
+import { Search, MoreVertical } from "lucide-react";
 import { Input } from "@/components/ui/input";
 
 function desktopCard(index: number) {
@@ -68,12 +68,12 @@ export default function BookmarksSkeleton() {
         <div className="min-h-screen bg-background text-foreground">
             <div className="mx-auto p-4">
                 <div className="relative mb-6">
-                    <div className="flex flex-col md:flex-row gap-4">
+                    <div className="flex flex-row gap-2 md:gap-4">
                         <Button
                             variant="outline"
                             size="lg"
                             className={
-                                "w-auto md:h-auto flex items-center justify-center"
+                                "hidden md:flex w-auto md:h-auto flex items-center justify-center"
                             }
                             disabled={true}
                         >
@@ -83,23 +83,31 @@ export default function BookmarksSkeleton() {
                             variant="outline"
                             size="lg"
                             className={
-                                "w-auto md:h-auto flex items-center justify-center bg-blue-500 dark:bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-600 text-white"
+                                "hidden md:flex w-auto md:h-auto flex items-center justify-center bg-blue-500 dark:bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-600 text-white"
                             }
                             disabled={true}
                         >
                             Sync to MAL
                         </Button>
-                        <div className="relative w-full">
+                        <div className="relative w-full h-10 md:h-9">
                             <Input
                                 type="search"
                                 placeholder={
                                     "Loading bookmarks, please wait..."
                                 }
                                 disabled={true}
-                                className="no-cancel"
+                                className="no-cancel text-sm h-full"
                             />
                             <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-muted-foreground" />
                         </div>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="flex size-10 md:hidden"
+                            disabled={true}
+                        >
+                            <MoreVertical className="h-4 w-4" />
+                        </Button>
                     </div>
                 </div>
                 <div className="grid grid-cols-1 2xl:grid-cols-2 gap-4 xl:gap-6">
