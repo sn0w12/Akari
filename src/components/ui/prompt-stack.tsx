fec81e895f929028edad3dbd55ced17fe12066eb
Lucas Sköld <lucasskold49@gmail.com>
2025-11-28 20:07:10 +0100

    Add slight bottom offset for prompt stack

--- src/components/ui/prompt-stack.tsx
+++ src/components/ui/prompt-stack.tsx
@@ -8,7 +8,7 @@ interface PromptStackProps {
 
 export function PromptStack({ children }: PromptStackProps) {
     return (
-        <div className="fixed bottom-0 left-0 right-0 md:bottom-4 md:right-4 md:left-auto z-50 flex flex-col-reverse gap-4">
+        <div className="fixed bottom-2 left-0 right-0 md:bottom-4 md:right-4 md:left-auto z-50 flex flex-col-reverse gap-4">
             {children}
         </div>
     );
