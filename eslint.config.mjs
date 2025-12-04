f178f4c5c997e7bc16a79981fcb3f7b764627d7e
Sn0w123 <lucasskold49@gmail.com>
2025-12-04 20:06:20 +0100

    Add custom eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,30 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+import noLocalStorage from "./eslint-rules/no-localstorage.mjs";
+
 const eslintConfig = defineConfig([
     ...nextVitals,
     ...nextTs,
+    {
+        rules: {
+            "custom/no-localstorage": "warn",
+        },
+        plugins: {
+            custom: {
+                rules: {
+                    "no-localstorage": noLocalStorage,
+                },
+            },
+        },
+    },
+    {
+        // Disable the rule in storage.ts to avoid false positives
+        files: ["src/lib/storage.ts"],
+        rules: {
+            "custom/no-localstorage": "off",
+        },
+    },
     // Override default ignores of eslint-config-next.
     globalIgnores([
         // Default ignores of eslint-config-next:
