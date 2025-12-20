b1670f2d4e267bfd2d553e81b9ac4cdc2fc73ea5
Sn0w123 <lucasskold49@gmail.com>
2025-12-20 15:55:37 +0100

    Revalidate home page on every revalidation request

--- src/app/api/v1/revalidate/route.ts
+++ src/app/api/v1/revalidate/route.ts
@@ -36,6 +36,7 @@ export async function POST(req: NextRequest) {
             revalidatePath(page);
         }
         revalidateTag("home", "max");
+        revalidatePath("/");
         return NextResponse.json({ revalidated: true });
     } catch {
         return NextResponse.json(
