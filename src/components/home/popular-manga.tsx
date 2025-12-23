2232aeb679e93af6a5ae05a29e85d7feb6f62437
Lucas Sköld <lucasskold49@gmail.com>
2025-12-23 02:54:54 +0100

    Make first 6 priority

--- src/components/home/popular-manga.tsx
+++ src/components/home/popular-manga.tsx
@@ -34,7 +34,7 @@ export function PopularManga({ manga }: PopularMangaProps) {
         <div>
             <div className={GRID_CLASS}>
                 {paginatedPopularList.map((manga, index) => (
-                    <MangaCard key={index} manga={manga} priority={index < 4} />
+                    <MangaCard key={index} manga={manga} priority={index < 6} />
                 ))}
             </div>
 
