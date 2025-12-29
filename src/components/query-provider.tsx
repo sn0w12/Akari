1a6933c8d5f017f698a50025716966202463ace3
Sn0w123 <lucasskold49@gmail.com>
2025-12-30 00:35:37 +0100

    Move queryClient to useState

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface QueryProviderProps {
     children: ReactNode;
 }
 
 export function QueryProvider({ children }: QueryProviderProps) {
-    const queryClient = new QueryClient();
+    const [queryClient] = useState(() => new QueryClient());
 
     return (
         <QueryClientProvider client={queryClient}>
