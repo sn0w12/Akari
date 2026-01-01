93e877462500a8aa269dec6b17932a62ff12f96e
Sn0w123 <lucasskold49@gmail.com>
2026-01-01 22:28:15 +0100

    Put popular manga in a carousel

--- src/components/grid-page.tsx
+++ src/components/grid-page.tsx
@@ -13,7 +13,7 @@ interface PageProps {
 }
 
 export const GRID_CLASS =
-    "grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-6";
+    "grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4";
 
 export default async function GridPage({
     title,
--- src/components/home/popular-manga.tsx
+++ src/components/home/popular-manga.tsx
@@ -1,49 +1,169 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { MangaCard } from "../manga/manga-card";
-import ClientPagination from "../ui/pagination/client-pagination";
-import { GRID_CLASS } from "../grid-page";
+import { generateSizes } from "@/lib/utils";
+import {
+    useCarousel,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from "../ui/carousel";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
 
 interface PopularMangaProps {
     manga: components["schemas"]["MangaResponse"][];
 }
 
 export function PopularManga({ manga }: PopularMangaProps) {
-    const [currentPopularPage, setCurrentPopularPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(12);
+    return (
+        <Carousel
+            opts={{
+                align: "start",
+                loop: true,
+            }}
+            className="w-full"
+        >
+            <CarouselContent>
+                {manga.map((mangaItem, index) => (
+                    <CarouselItem
+                        key={mangaItem.id}
+                        className="pl-4 2xl:basis-1/2"
+                    >
+                        <PopularMangaCard
+                            manga={mangaItem}
+                            priority={index <= 1}
+                        />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselControls />
+        </Carousel>
+    );
+}
+
+function CarouselControls() {
+    const { scrollNext, canScrollNext, scrollPrev, canScrollPrev, api } =
+        useCarousel();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
 
     useEffect(() => {
-        const handleResize = () => {
-            setItemsPerPage(window.innerWidth < 640 ? 6 : 12);
+        if (!api) return;
+
+        const onSelect = () => {
+            setCurrentPage(api.selectedScrollSnap() + 1);
+            setTotalPages(api.scrollSnapList().length);
         };
 
-        handleResize();
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+        onSelect(); // Initial update
+        api.on("select", onSelect);
 
-    const totalPopularPages = Math.ceil(manga.length / itemsPerPage);
-
-    const paginatedPopularList = manga.slice(
-        (currentPopularPage - 1) * itemsPerPage,
-        currentPopularPage * itemsPerPage
-    );
+        return () => {
+            api.off("select", onSelect);
+        };
+    }, [api]);
 
     return (
-        <div>
-            <div className={GRID_CLASS}>
-                {paginatedPopularList.map((manga, index) => (
-                    <MangaCard key={index} manga={manga} priority={index < 6} />
-                ))}
-            </div>
-
-            <ClientPagination
-                currentPage={currentPopularPage}
-                totalPages={totalPopularPages}
-                handlePageChange={setCurrentPopularPage}
-                className="mt-4"
-            />
+        <div className="flex items-center gap-2 pt-1 w-full justify-between sm:p-0 sm:w-auto sm:absolute sm:bottom-2 sm:right-2">
+            <span className="order-2 sm:order-1 text-sm text-muted-foreground">
+                {currentPage} / {totalPages}
+            </span>
+            <Button
+                size="icon"
+                className="order-1 sm:order-2"
+                disabled={!canScrollPrev}
+                onClick={scrollPrev}
+            >
+                <ArrowLeft />
+                <span className="sr-only">Previous slide</span>
+            </Button>
+            <Button
+                size="icon"
+                className="order-3"
+                disabled={!canScrollNext}
+                onClick={scrollNext}
+            >
+                <ArrowRight />
+                <span className="sr-only">Next slide</span>
+            </Button>
         </div>
+    );
+}
+
+function PopularMangaCardInfo({
+    label,
+    value,
+}: {
+    label: string;
+    value: string;
+}) {
+    return (
+        <div className="space-y-1">
+            <p className="text-sm font-medium text-muted-foreground">{label}</p>
+            <p className="line-clamp-1 text-lg text-foreground">{value}</p>
+        </div>
+    );
+}
+
+interface PopularMangaCardProps {
+    manga: components["schemas"]["MangaResponse"];
+    priority?: boolean;
+}
+
+function PopularMangaCard({ manga, priority }: PopularMangaCardProps) {
+    return (
+        <Link
+            href={`/manga/${manga.id}`}
+            className="flex flex-row h-full w-full rounded-lg border"
+        >
+            <Image
+                src={manga.cover}
+                alt={manga.title}
+                className="h-auto w-full sm:w-64 object-cover rounded-l-lg rounded-r-lg sm:rounded-r-none"
+                width={200}
+                height={300}
+                quality={80}
+                loading={priority ? "eager" : "lazy"}
+                fetchPriority={priority ? "high" : "auto"}
+                preload={priority}
+                sizes={generateSizes({
+                    sm: "640px",
+                    md: "256px",
+                })}
+            />
+            <div className="space-y-2 py-2 px-4 w-full hidden sm:block">
+                <h2 className="line-clamp-2 text-3xl font-semibold leading-tight text-card-foreground border-b pb-1">
+                    {manga.title}
+                </h2>
+
+                <PopularMangaCardInfo
+                    label="Author"
+                    value={manga.authors.join(", ")}
+                />
+                <PopularMangaCardInfo label="Status" value={manga.status} />
+                <PopularMangaCardInfo label="Type" value={manga.type} />
+                <div className="space-y-1">
+                    <p className="text-sm font-medium text-muted-foreground">
+                        Genres
+                    </p>
+                    <div className="flex flex-wrap gap-1">
+                        {manga.genres.slice(0, 6).map((genre) => (
+                            <Badge variant="secondary" key={genre}>
+                                {genre}
+                            </Badge>
+                        ))}
+                        {manga.genres.length > 6 && (
+                            <Badge variant="secondary">
+                                +{manga.genres.length - 6}
+                            </Badge>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </Link>
     );
 }
