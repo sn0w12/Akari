6a38bd5b1dfa57b6ec094cfe0128243c69b7b2af
Sn0w123 <lucasskold49@gmail.com>
2026-01-01 15:50:50 +0100

    Update auth pages to no longer overflow

--- src/app/(default)/auth/error/page.tsx
+++ src/app/(default)/auth/error/page.tsx
@@ -25,7 +25,7 @@ export default function Page({
     searchParams: Promise<{ error: string }>;
 }) {
     return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="flex h-full w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-6">
                     <Card>
--- src/app/(default)/auth/sign-up-success/page.tsx
+++ src/app/(default)/auth/sign-up-success/page.tsx
@@ -8,7 +8,7 @@ import {
 
 export default function Page() {
     return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div className="flex h-full w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-6">
                     <Card>
