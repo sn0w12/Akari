a5fc4f8bc10384469d180d462617ec83bb817c52
Lucas Sköld <lucasskold49@gmail.com>
2026-01-07 21:49:47 +0100

    Use plausible for analytics

--- src/components/analytics/analytics-wrapper.tsx
+++ src/components/analytics/analytics-wrapper.tsx
@@ -1,22 +1,45 @@
 "use client";
 
+import { useEffect } from "react";
 import { useCookieConsent } from "@/hooks/use-cookie-consent";
-import Script from "next/script";
+import { useUser } from "@/contexts/user-context";
 
 export function AnalyticsWrapper() {
     const { consent } = useCookieConsent();
+    const { user, isLoading } = useUser();
+    const domain = process.env.NEXT_PUBLIC_HOST;
+    const endpoint = process.env.NEXT_PUBLIC_PLAUSIBLE_ENDPOINT;
 
-    return (
-        <>
-            {consent.analytics && (
-                <Script
-                    defer
-                    src="https://cloud.umami.is/script.js"
-                    data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-                    data-exclude-search="true"
-                    strategy="afterInteractive"
-                />
-            )}
-        </>
-    );
+    useEffect(() => {
+        const loadAndInit = async () => {
+            if (!domain) {
+                console.warn("Plausible URL not set in environment variables.");
+                return;
+            }
+            if (!endpoint) {
+                console.warn(
+                    "Plausible endpoint not set in environment variables."
+                );
+                return;
+            }
+
+            if (!consent.analytics) return;
+            if (isLoading) return;
+
+            const { init } = await import("@plausible-analytics/tracker");
+
+            init({
+                domain: domain,
+                endpoint: endpoint,
+                outboundLinks: true,
+                customProperties: {
+                    logged_in: (!!user).toString(),
+                },
+            });
+        };
+
+        loadAndInit();
+    }, [domain, endpoint, consent.analytics, user, isLoading]);
+
+    return null;
 }
