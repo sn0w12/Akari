5ebcdb0e1603c93391537abba4afa6714edad5cb
Lucas Sköld <lucasskold49@gmail.com>
2026-01-22 00:00:31 +0100

    Fix strip progress

--- src/components/manga-reader/strip-page-progress.tsx
+++ src/components/manga-reader/strip-page-progress.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useRef } from "react";
 import { useSetting } from "@/lib/settings";
+import { cn } from "@/lib/utils";
+import React, { useRef } from "react";
 
 interface PageProgressProps {
     progress: number;
@@ -19,13 +20,17 @@ export default function StripPageProgress({
 
     return (
         <div
-            className={`${
-                isVisible && !hidden ? "opacity-100" : "opacity-0"
-            } flex transition-opacity fixed z-50 left-4 right-4 bottom-4 lg:left-auto lg:right-4 lg:top-auto`}
+            className={cn(
+                "flex transition-opacity fixed z-50 left-4 right-4 bottom-4 lg:left-auto lg:right-4 lg:top-auto w-[calc(100%-118px)]",
+                {
+                    "opacity-100": isVisible && !hidden,
+                    "opacity-0 pointer-events-none": !isVisible || hidden,
+                },
+            )}
         >
             <div
                 ref={containerRef}
-                className="transition-[width] relative p-1 rounded-lg border border-primary/30 bg-transparent h-7.5 w-[calc(100%-118px)] lg:w-9 lg:h-[75vh]"
+                className="transition-[width] relative p-1 rounded-lg border border-primary/30 bg-transparent h-7.5 w-full lg:w-9 lg:h-[75vh]"
                 style={
                     {
                         "--progress": `${progress * 100}%`,
@@ -34,7 +39,10 @@ export default function StripPageProgress({
                 onClick={(e) => e.stopPropagation()}
             >
                 <div
-                    className={`absolute left-1 top-1 lg:top-1 right-1 lg:right-1 transition-[height,width] rounded-sm ${gradient} progress-bar`}
+                    className={cn(
+                        "absolute left-1 top-1 lg:top-1 right-1 lg:right-1 transition-[height,width] rounded-sm progress-bar",
+                        gradient,
+                    )}
                 />
             </div>
         </div>
