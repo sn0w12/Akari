bdfdbf70c8f5bf1870f9227952deb1db02035f62
Sn0w123 <lucasskold49@gmail.com>
2026-01-26 21:18:50 +0100

    Improve bookmark button by only taking mangaId

--- src/components/manga-details/buttons.tsx
+++ src/components/manga-details/buttons.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { useUser } from "@/contexts/user-context";
-import { checkIfBookmarked } from "@/lib/manga/bookmarks";
-import { useQuery } from "@tanstack/react-query";
 import { ListSelector } from "../list/list-selector";
-import { Skeleton } from "../ui/skeleton";
 import BookmarkButton from "./bookmark-button";
 
 interface ButtonsProps {
@@ -12,28 +8,9 @@ interface ButtonsProps {
 }
 
 export default function Buttons({ manga }: ButtonsProps) {
-    const { user } = useUser();
-    const { data: isBookmarked, isLoading } = useQuery({
-        queryKey: ["bookmark", manga.id],
-        queryFn: () => checkIfBookmarked(manga.id),
-        enabled: !!manga.id && !!user,
-    });
-
-    if (isLoading) {
-        return (
-            <div className="flex flex-col xl:flex-row gap-2">
-                <Skeleton className="h-11 w-full xl:w-1/2" />
-                <Skeleton className="h-11 w-full xl:w-1/2" />
-            </div>
-        );
-    }
-
     return (
         <div className="flex flex-col xl:flex-row gap-2">
-            <BookmarkButton
-                manga={manga}
-                isBookmarked={isBookmarked ?? false}
-            />
+            <BookmarkButton mangaId={manga.id} />
             <ListSelector mangaId={manga.id} />
         </div>
     );
