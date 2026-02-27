55f3f35a69573c420acd7a0affdb16884cc5bd0e
Sn0w123 <lucasskold49@gmail.com>
2026-02-27 18:57:05 +0100

    Revert "Add optional NEXT_PRIVATE_SUPABASE_URL"
    This reverts commit 4ded5872394405b5bb1513ec5898a4db6807564f.

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -5,8 +5,7 @@ export async function createClient() {
     const cookieStore = await cookies();
 
     return createServerClient(
-        process.env.NEXT_PRIVATE_SUPABASE_URL ??
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
         {
             cookies: {
