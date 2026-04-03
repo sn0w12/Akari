f5680dc4a1291cc533eb6205a0576186d9c0391c
Sn0w123 <lucasskold49@gmail.com>
2026-04-03 15:26:36 +0200

    re-enable view transitions on mobile

--- src/components/view-transition.tsx
+++ src/components/view-transition.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import { useDevice } from "@/contexts/device-context";
-import { useMemo, ViewTransition, ViewTransitionProps } from "react";
+import { ViewTransition, ViewTransitionProps } from "react";
 
 interface CustomViewTransitionProps extends ViewTransitionProps {
     children: React.ReactNode;
@@ -11,18 +8,5 @@ export function CustomViewTransition({
     children,
     ...props
 }: CustomViewTransitionProps) {
-    const { os, isPWA } = useDevice();
-    const enabled = useMemo(() => {
-        if (os === "iOS" || isPWA) {
-            return false;
-        }
-
-        return true;
-    }, [os, isPWA]);
-
-    if (enabled) {
-        return <ViewTransition {...props}>{children}</ViewTransition>;
-    }
-
-    return <>{children}</>;
+    return <ViewTransition {...props}>{children}</ViewTransition>;
 }
