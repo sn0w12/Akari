5c40cfe45916715933ff09282bfc31ec1ee670ab
Sn0w123 <lucasskold49@gmail.com>
2026-05-21 15:06:45 +0200

    Fix scrollToTopSelectors

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,8 @@ import { routeTree } from "./routeTree.gen";
 export function getRouter() {
     const router = createRouter({
         routeTree,
-        scrollRestoration: false,
+        scrollRestoration: true,
+        scrollToTopSelectors: ["#scroll-element"],
         defaultPreload: "intent",
         defaultNotFoundComponent: () => <p>Page not found</p>,
     });
