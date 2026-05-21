8965e72795d971824270174c920f5cf15e67bb28
Sn0w123 <lucasskold49@gmail.com>
2026-05-21 15:14:01 +0200

    Add missing body classes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,11 @@
-import type { ReactNode } from "react";
+import "@/globals.css";
 import {
     HeadContent,
     Outlet,
     Scripts,
     createRootRoute,
 } from "@tanstack/react-router";
-import "@/globals.css";
+import type { ReactNode } from "react";
 
 export const Route = createRootRoute({
     head: () => ({
@@ -13,7 +13,8 @@ export const Route = createRootRoute({
             { charSet: "utf-8" },
             {
                 name: "viewport",
-                content: "width=device-width, initial-scale=1, viewport-fit=cover",
+                content:
+                    "width=device-width, initial-scale=1, viewport-fit=cover",
             },
             { title: "Akari Manga" },
         ],
@@ -35,7 +36,7 @@ function RootDocument({ children }: { children: ReactNode }) {
             <head>
                 <HeadContent />
             </head>
-            <body className="antialiased bg-background">
+            <body className="antialiased bg-background md:h-screen flex flex-col antialiased bg-background overflow-y-auto pt-[var(--safe-top)]! md:pt-0! mb-[var(--header-height)] md:mb-0 md:overflow-hidden">
                 {children}
                 <Scripts />
             </body>
