4faf7e47b3216c18a961f61a4edbea272776687b
Sn0w123 <lucasskold49@gmail.com>
2026-06-01 02:15:36 +0200

    Improve list select

---
+++ src/components/list/list-create.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {
+    ResponsiveModal,
+    ResponsiveModalHeader,
+    ResponsiveModalPanel,
+    ResponsiveModalPopup,
+    ResponsiveModalTitle,
+    ResponsiveModalTrigger,
+} from "../ui/responsive-modal";
+import { Button } from "../ui/button";
+import { CreateListForm } from "../account/create-list-form";
+
+interface ListCreateProps {
+    setLists?: React.Dispatch<
+        React.SetStateAction<components["schemas"]["UserMangaListResponse"][]>
+    >;
+    onSuccess?: (list: components["schemas"]["UserMangaListResponse"]) => void;
+    className?: string;
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    showTrigger?: boolean;
+}
+
+export function ListCreate({
+    setLists,
+    onSuccess,
+    className,
+    open: openProp,
+    onOpenChange,
+    showTrigger = true,
+}: ListCreateProps) {
+    const [internalOpen, setInternalOpen] = useState(false);
+    const open = openProp ?? internalOpen;
+
+    function setOpen(nextOpen: boolean) {
+        if (openProp === undefined) {
+            setInternalOpen(nextOpen);
+        }
+
+        onOpenChange?.(nextOpen);
+    }
+
+    function handleSuccess(
+        list: components["schemas"]["UserMangaListResponse"],
+    ) {
+        if (setLists) {
+            setLists((prev) => [...prev, list]);
+        }
+        onSuccess?.(list);
+        setOpen(false);
+    }
+
+    return (
+        <ResponsiveModal open={open} onOpenChange={setOpen}>
+            {showTrigger ? (
+                <ResponsiveModalTrigger
+                    render={<Button variant="outline" className={className} />}
+                >
+                    Create List
+                </ResponsiveModalTrigger>
+            ) : null}
+            <ResponsiveModalPopup>
+                <ResponsiveModalHeader>
+                    <ResponsiveModalTitle>Create New List</ResponsiveModalTitle>
+                </ResponsiveModalHeader>
+                <ResponsiveModalPanel>
+                    <CreateListForm
+                        onSuccess={handleSuccess}
+                        onClose={() => setOpen(false)}
+                    />
+                </ResponsiveModalPanel>
+            </ResponsiveModalPopup>
+        </ResponsiveModal>
+    );
+}
