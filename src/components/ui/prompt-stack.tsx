a331cbbcfd172d441dad0b156f4af17a537c4de4
Sn0w123 <lucasskold49@gmail.com>
2026-06-04 14:54:04 +0200

    Fix prompt stack offset

--- src/components/ui/prompt-stack.tsx
+++ src/components/ui/prompt-stack.tsx
@@ -7,7 +7,7 @@ interface PromptStackProps {
 
 export function PromptStack({ children }: PromptStackProps) {
     return (
-        <div className="fixed bottom-2 left-0 right-0 md:bottom-4 md:right-4 md:left-auto z-50 flex flex-col-reverse gap-4">
+        <div className="fixed bottom-[calc(var(--safe-bottom)+--spacing(16))] left-2 right-2 md:bottom-4 md:right-4 md:left-auto z-50 flex flex-col-reverse gap-4">
             {children}
         </div>
     );
